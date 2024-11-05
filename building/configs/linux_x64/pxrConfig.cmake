bf45f0f034a8ef1b473e65cbe745c372f5302e69
cjhosken <hoskenchristopher@gmail.com>
2024-11-05 01:24:34 +0000

    fixed crashing

--- building/configs/linux_x64/pxrConfig.cmake
+++ building/configs/linux_x64/pxrConfig.cmake
@@ -25,8 +25,86 @@ set(PXR_usd_ms_LIBRARY "${USD_ROOT}/lib/libusd_ms.so")
 # Set include directories
 set(PXR_INCLUDE_DIRS "${USD_ROOT}/include" CACHE PATH "Path to the pxr include directory")
 
+# Define interface targets for each library
+add_library(arch INTERFACE IMPORTED)
+set_target_properties(arch PROPERTIES IMPORTED_LOCATION "${PXR_usd_ms_LIBRARY}")
+
+add_library(tf INTERFACE IMPORTED)
+set_target_properties(tf PROPERTIES IMPORTED_LOCATION "${PXR_usd_ms_LIBRARY}")
+
+add_library(gf INTERFACE IMPORTED)
+set_target_properties(gf PROPERTIES IMPORTED_LOCATION "${PXR_usd_ms_LIBRARY}")
+
+add_library(vt INTERFACE IMPORTED)
+set_target_properties(vt PROPERTIES IMPORTED_LOCATION "${PXR_usd_ms_LIBRARY}")
+
+add_library(ndr INTERFACE IMPORTED)
+set_target_properties(ndr PROPERTIES IMPORTED_LOCATION "${PXR_usd_ms_LIBRARY}")
+
+add_library(sdr INTERFACE IMPORTED)
+set_target_properties(sdr PROPERTIES IMPORTED_LOCATION "${PXR_usd_ms_LIBRARY}")
+
+add_library(sdf INTERFACE IMPORTED)
+set_target_properties(sdf PROPERTIES IMPORTED_LOCATION "${PXR_usd_ms_LIBRARY}")
+
+add_library(usd INTERFACE IMPORTED)
+set_target_properties(usd PROPERTIES IMPORTED_LOCATION "${PXR_usd_ms_LIBRARY}")
+
+add_library(ar INTERFACE IMPORTED)
+set_target_properties(ar PROPERTIES IMPORTED_LOCATION "${PXR_usd_ms_LIBRARY}")
+
+add_library(plug INTERFACE IMPORTED)
+set_target_properties(plug PROPERTIES IMPORTED_LOCATION "${PXR_usd_ms_LIBRARY}")
+
+add_library(trace INTERFACE IMPORTED)
+set_target_properties(trace PROPERTIES IMPORTED_LOCATION "${PXR_usd_ms_LIBRARY}")
+
+add_library(work INTERFACE IMPORTED)
+set_target_properties(work PROPERTIES IMPORTED_LOCATION "${PXR_usd_ms_LIBRARY}")
+
+add_library(hf INTERFACE IMPORTED)
+set_target_properties(hf PROPERTIES IMPORTED_LOCATION "${PXR_usd_ms_LIBRARY}")
+
+add_library(hd INTERFACE IMPORTED)
+set_target_properties(hd PROPERTIES IMPORTED_LOCATION "${PXR_usd_ms_LIBRARY}")
+
+add_library(usdGeom INTERFACE IMPORTED)
+set_target_properties(usdGeom PROPERTIES IMPORTED_LOCATION "${PXR_usd_ms_LIBRARY}")
+
+add_library(usdImaging INTERFACE IMPORTED)
+set_target_properties(usdImaging PROPERTIES IMPORTED_LOCATION "${PXR_usd_ms_LIBRARY}")
+
+add_library(usdLux INTERFACE IMPORTED)
+set_target_properties(usdLux PROPERTIES IMPORTED_LOCATION "${PXR_usd_ms_LIBRARY}")
+
+add_library(usdShade INTERFACE IMPORTED)
+set_target_properties(usdShade PROPERTIES IMPORTED_LOCATION "${PXR_usd_ms_LIBRARY}")
+
+add_library(pxOsd INTERFACE IMPORTED)
+set_target_properties(pxOsd PROPERTIES IMPORTED_LOCATION "${PXR_usd_ms_LIBRARY}")
+
+add_library(cameraUtil INTERFACE IMPORTED)
+set_target_properties(cameraUtil PROPERTIES IMPORTED_LOCATION "${PXR_usd_ms_LIBRARY}")
+
+add_library(pcp INTERFACE IMPORTED)
+set_target_properties(pcp PROPERTIES IMPORTED_LOCATION "${PXR_usd_ms_LIBRARY}")
+
+add_library(usdUtils INTERFACE IMPORTED)
+set_target_properties(usdUtils PROPERTIES IMPORTED_LOCATION "${PXR_usd_ms_LIBRARY}")
+
+add_library(usdVol INTERFACE IMPORTED)
+set_target_properties(usdUtils PROPERTIES IMPORTED_LOCATION "${PXR_usd_ms_LIBRARY}")
+
+add_library(usdSkel INTERFACE IMPORTED)
+set_target_properties(usdUtils PROPERTIES IMPORTED_LOCATION "${PXR_usd_ms_LIBRARY}")
+
+add_library(usdRender INTERFACE IMPORTED)
+set_target_properties(usdRender PROPERTIES IMPORTED_LOCATION "${PXR_usd_ms_LIBRARY}")
+
 # Initialize PXR_LIBRARIES with the path to the monolithic library
 set(PXR_LIBRARIES "${PXR_usd_ms_LIBRARY}")
+
+
 
 # If PXR_STATIC is defined, include Windows-specific libraries
 if(NOT ON)
